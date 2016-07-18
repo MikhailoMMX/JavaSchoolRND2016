d935eba1bad6cadaa2adad152f30d2bc58aa0ebe
MikhailoMMX <mmxforever@mail.ru>
2016-07-18 14:12:33 +0300

    Homework 2

---
+++ HomeWork_1/src/MikhailoMMX/Task2.java
@@ -0,0 +1,35 @@
+package MikhailoMMX;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by MikhailoMMX on 17.07.2016.
+ */
+public class Task2 {
+    public static final String inputFile = "2061_in.txt";
+    public static void  main(String [] args){
+        if (!Files.exists(Paths.get(inputFile))){
+            System.out.println("Error: input file does not exist");
+            return;
+        }
+        try {
+            List<String> inputStrings = Files.readAllLines(Paths.get(inputFile));
+            ArrayList<String> outputStrings = new ArrayList<>();
+            for (String str: inputStrings)
+                outputStrings.add(str.replaceAll(" ", ""));
+            Collections.sort(outputStrings);
+            for (String str: outputStrings)
+                System.out.println(str);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Exception: " + e.toString());
+        }
+    }
+}
